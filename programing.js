2c879958b4844c3c63ceb4f0b4e12097607641c3
Joshua Nivar <joshuanivar16@gmail.com>
2024-09-17 20:25:44 -0400

    segundo commit

--- programing.js
+++ programing.js
@@ -1,59 +1,71 @@
+console.log("\nReglas del juego:\n");
+console.log("1. El jugador debe de ingresar hasta qué cantidad de números desea contar");
+console.log("2. Mientras el jugador esté contando debe de sustituir el número divisible entre 3 por la palabra 'fizz'");
+console.log("3. Si el número es divisible entre 5 debe de sustituirlo por la palabra 'buzz'");
+console.log("4. Si el número es divisible entre 3 y 5 el jugador debe de escribir 'fizzbuzz'")
+
 const prompt = require('prompt-sync')();
-let counterOne = 0;
 let increase = 1;
 
-console.log("Ingresa hasta qué cantidad desea contar");
+process.stdout.write("\nIngresa hasta qué cantidad desea contar :\n");
 let input = prompt();
 let arrayNumbers = new Array(input);
 
-while(arrayNumbers.length < input){
-    arrayNumbers[counterOne] = increase * 1;
-    counterOne +=1;
+for(let i = 0; i < input; i++){
+    arrayNumbers[i] = increase;
     increase += 1;
 }
 
 let results = [];
 let counterTwo = 0;
-
+console.log("----GAME START----\n");
+outerLoop: //etiqueta para poder salir de varios niveles de bucles
 for (let i of arrayNumbers){
     let input = prompt();
     results.push(input.toLocaleLowerCase());
 
-    if (i % 3 === 0 && i % 5 === 0 && results[counterTwo] === "fizzbuzz"){
-        counterTwo += 1;
+    
+    switch(true){
+          //El usuario pierde si no reemplaza las palabras por los númneros correspondientes
+          case (parseInt(input) % 3 === 0 && parseInt(input) % 5 === 0):
+            console.log("Game over 0");
+            break outerLoop;
+        case (parseInt(input) % 3 === 0):
+            console.log("Game over 1");
+            break outerLoop;
+        case (parseInt(input) % 5 === 0):
+            console.log("Game over 2");
+            break outerLoop;
+        //Asegurando cada caso "fizzbuzz, fizz y buzz"
+        case (i % 3 === 0 && i % 5 === 0 && results[counterTwo] === "fizzbuzz"):
+            counterTwo += 1;
+            break;
+        case (i % 5 === 0 && results[counterTwo]==="buzz"):
+            counterTwo += 1;
+            break;
+        case (i % 3 === 0 && results[counterTwo] === "fizz"):
+            counterTwo += 1;
+            break;
+               //Asegurando que el usuario siga la sencuencia de números correctamente
+        case (parseInt(input) != i):
+            console.log("Game over 3")
+            break outerLoop;
+          
+        default:
+            counterTwo += 1;
+            break;
+                
     }
-    else  if (i % 5 === 0 && results[counterTwo] === "buzz"){
-        counterTwo += 1;
-    }
-    else  if (i % 3 === 0 && results[counterTwo] === "fizz"){
-        counterTwo += 1;
-    }
-    else  if (parseInt(input) % 3 === 0 && parseInt(input) % 5 === 0 && results[counterTwo] != "fizzbuzz"){
-        console.log("Game over 0");
-        break;
-    }
-    else  if (parseInt(input) % 3 === 0 && results[counterTwo] != "fizz"){
-        console.log("Game over 1");
-        break;
-    }
-    else  if (parseInt(input) % 5 === 0 && results[counterTwo] != "buzz"){
-        console.log("Game over 2");
-        break;
-    }
-    else if(isNaN(input)){
-        console.log("Game over 3")
-        break;
-    }
-    else if (parseInt(input) != i){
-        console.log("Número incorrecto... Game over");
-        break;
-    }
-    else counterTwo += 1;
 
 }
 
 
 console.log("\nResultados\n");
 for (let i of results){
-    process.stdout.write(" "+i);
+    process.stdout.write(" "+i +"");
+}
+
+function game(){
+    
+    
 }
